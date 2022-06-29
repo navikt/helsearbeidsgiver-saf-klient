00e70f6362b41d9a7e6c8e43d76fba6407273bf2
Gustav Berggren <gustav.berggren@nav.no>
2022-06-29 13:19:26 +0200

    Fiks feil i tester

--- src/test/kotlin/no/nav/helsearbeidsgiver/saf/client/SafKlientTest.kt
+++ src/test/kotlin/no/nav/helsearbeidsgiver/saf/client/SafKlientTest.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.json.Json
 import no.nav.helsearbeidsgiver.saf.client.graphql.ErrorException
 import no.nav.helsearbeidsgiver.saf.client.graphql.SafDokumentoversiktBrukerException
 import no.nav.helsearbeidsgiver.saf.graphql.generated.enums.BrukerIdType
+import no.nav.helsearbeidsgiver.saf.utils.MDCOperations
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
@@ -16,23 +17,23 @@ class SafKlientTest {
     fun `Forventer gyldig response fra DokumentoversiktFagsak`() {
         val response = Json.encodeToString(lagGyldigResponse())
         val safKlient = buildSafKlient(response)
-        val resultat = safKlient.dokumentoversiktFagsakSync("12345", "Test")
+        val resultat = safKlient.dokumentoversiktFagsakSync("12345", "Test", MDCOperations.generateCallId())
         assertEquals("1234", resultat.get(0)?.journalpostId)
     }
     @Test
     fun `Forventer gyldig response fra DokumentoversiktBruker`() {
         val response = Json.encodeToString(lagGyldigDokumentBrukerResponse())
         val safKlient = buildSafKlient(response)
-        val resultat = safKlient.dokumentoversiktBrukerSync("12345", BrukerIdType.AKTOERID)
+        val resultat = safKlient.dokumentoversiktBrukerSync("12345", BrukerIdType.AKTOERID, MDCOperations.generateCallId())
         assertEquals("1234", resultat.get(0)?.journalpostId)
     }
 
     @Test
-    fun `Forventer gyldig feilmelding`() {
+    fun `Forventer gyldig feilmelding for dokumentFagsak`() {
         val response = getResourceAsText("error.json")
         val safKlient = buildSafKlient(response)
         assertThrows<ErrorException> {
-            safKlient.dokumentoversiktFagsakSync("12345", "Test")
+            safKlient.dokumentoversiktFagsakSync("12345", "Test", MDCOperations.generateCallId())
         }
     }
     @Test
@@ -40,7 +41,7 @@ class SafKlientTest {
         val response = getResourceAsText("error.json")
         val safKlient = buildSafKlient(response)
         assertThrows<SafDokumentoversiktBrukerException> {
-            safKlient.dokumentoversiktBrukerSync("12345", BrukerIdType.AKTOERID)
+            safKlient.dokumentoversiktBrukerSync("12345", BrukerIdType.AKTOERID, MDCOperations.generateCallId())
         }
     }
 }
